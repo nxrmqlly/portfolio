528cbfb192b9a461b1a0de8fb6d0d306d730820b
nxrmqlly <ritam.das3110@outlook.com>
2025-01-23 20:01:11 +0530

    hotfix: fix component name v2 lol

--- src/routes/wip/+page.svelte
+++ src/routes/wip/+page.svelte
@@ -1,21 +1,19 @@
 <script>
-    import NavLink from "$lib/components/navbar/NavLink1.svelte";
+	import NavLink from '$lib/components/navbar/NavLink.svelte';
 </script>
 
 <div class="container">
-    <NavLink href="/" text="home" />
+	<NavLink href="/" text="home" />
 </div>
 
 <style>
-    .container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        height: 100vh;
-        width: 100vw;
-        overflow: hidden;
-    }
-    
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+		width: 100vw;
+		overflow: hidden;
+	}
 </style>
-
