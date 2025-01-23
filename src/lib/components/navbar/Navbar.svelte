4d6b65ec90b34ca0c53ac5767a9eedf8587b6719
nxrmqlly <ritam.das3110@outlook.com>
2025-01-23 15:56:24 +0530

    add navbar

---
+++ src/lib/components/navbar/Navbar.svelte
@@ -0,0 +1,49 @@
+<script>
+	import {gsap} from '$lib/gsap';
+	import { onMount } from 'svelte';
+	import NavOverlay from './NavOverlay.svelte';
+	const { route } = $props();
+
+	let navOpen = $state(false);
+
+	const toggleNav = () => {
+		navOpen = !navOpen;
+	};
+</script>
+
+<NavOverlay {navOpen} />
+
+<nav class="navbar">
+	<a href="/" class="nav-link">ritam das / {route}</a>
+	<button id="open-nav" class="nav-btn" onclick={toggleNav}>menu</button>
+</nav>
+
+<style lang="scss">
+	.navbar {
+		z-index: 5;
+		background-color: var(--color-ghost);
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: calc(100%);
+		height: 5vh;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	.nav-link,
+	.nav-btn {
+		border: none;
+		cursor: pointer;
+		padding: 1rem;
+		background: none;
+		font-size: 1.5rem;
+		text-decoration: none;
+		color: black;
+		font-family: 'Satoshi', sans-serif;
+	}
+	.nav-btn:hover {
+		background: none;
+	}
+</style>
